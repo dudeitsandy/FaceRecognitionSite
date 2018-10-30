b4a33b1fc53dcd268ac8c6163043c8314e40c788
LAPTOP-U0VJ5OVI\subfl <subflw47@gmail.com>
2018-10-29 23:31:17 -0500

    Push all core files pre-node integration

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+	return(
+		<div>
+			<div className ='white f2'>
+				{'Andy, your current rank is...'}
+			</div>
+			<div className ='white f1'>
+				{'#5'}
+			</div>
+		</div>
+	);
+}
+
+export default Rank;
